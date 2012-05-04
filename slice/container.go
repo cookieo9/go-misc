be6bda84e8facc6f961e555c562837b5191317ce
Carlos Castillo <cookieo9@gmail.com>
2012-05-04 09:44:41 -0700

    Added generic Insert & Delete slice functions.
    Currently only tests for insert.

---
+++ slice/container.go
@@ -0,0 +1,144 @@
+package slice
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+func check_insert(slice interface{}, idx int, item interface{}) (slice_val, item_val reflect.Value) {
+	slice_val = reflect.ValueOf(slice)
+	if slice_val.Kind() != reflect.Slice {
+		panic(errors.New("insert: argument 1 must be slice"))
+	}
+
+	item_val = reflect.ValueOf(item)
+	if item_val.Type() != slice_val.Type().Elem() {
+		panic(fmt.Errorf("insert: %T can't be inserted into %T", item, slice))
+	}
+
+	if idx < 0 || idx > slice_val.Len() {
+		panic(fmt.Errorf("insert: index (%d) out of range (0..%d)", idx, slice_val.Len()))
+	}
+
+	return
+}
+
+// Insert adds an element to a slice at a given index. If len(slice) < cap(slice),
+// then the original slice is expanded, otherwise a new slice is allocated.
+// This behaviour is analogous to builtin.append().
+//
+// The returned slice will reference the array where the insert occured, and
+// will have apropriate values for cap and len. The returned slice will be
+// the same type as the given slice, but stored in an interface{}.
+//
+// Uses reflection to perform this action on slices of any type.
+// Will panic if:
+//	- slice argument is not a slice type
+//	- slice argument's element type doesn't match item's type
+//	- idx is not in the range 0 .. len(slice)
+//
+// Equivalent to:
+//	slice = append(slice[:idx+1],slice[idx:]...)
+//	slice[idx] = item
+//
+// NOTE: Other slices referencing the same underlying array (including the argument
+// slice) may have their contents altered, if the change is made in place. To
+// guarantee that a new array will always be created use the InsertCopy function.
+func Insert(slice interface{}, idx int, item interface{}) interface{} {
+	slice_val, item_val := check_insert(slice, idx, item)
+
+	if idx == slice_val.Len() {
+		return reflect.Append(slice_val, item_val).Interface()
+	}
+
+	begin := slice_val.Slice(0, idx+1)
+	end := slice_val.Slice(idx, slice_val.Len())
+
+	out := reflect.AppendSlice(begin, end)
+	out.Index(idx).Set(item_val)
+	return out.Interface()
+}
+
+// Insert adds an element to a slice at a given index. Always allocates
+// a new slice, and never modifies the original memory.
+//
+// The returned slice will be the same type as the given slice, but
+// stored in an interface{}.
+//
+// Uses reflection to perform this action on slices of any type.
+// Will panic if:
+//	- slice argument is not a slice type
+//	- slice argument's element type doesn't match item's type
+//	- idx is not in the range 0 .. len(slice)
+//
+// Equivalent to:
+// 	begin, end := slice[:idx], slice[idx:]
+// 	slice = append(append(append(make([]T,0,len(slice)+1), begin...),item),end...)
+func InsertCopy(slice interface{}, idx int, item interface{}) interface{} {
+	slice_val, item_val := check_insert(slice, idx, item)
+	begin := slice_val.Slice(0, idx)
+	end := slice_val.Slice(idx, slice_val.Len())
+
+	out := reflect.MakeSlice(slice_val.Type(), 0, slice_val.Len()+1)
+	out = reflect.AppendSlice(reflect.Append(reflect.AppendSlice(out, begin), item_val), end)
+	return out.Interface()
+}
+
+func check_delete(slice interface{}, idx int) (slice_val reflect.Value) {
+	slice_val = reflect.ValueOf(slice)
+	if slice_val.Kind() != reflect.Slice {
+		panic(errors.New("delete: argument 1 must be slice"))
+	}
+
+	if idx < 0 || idx > slice_val.Len()-1 {
+		panic(fmt.Errorf("delete: idx (%d) out of range (0..%d)", idx, slice_val.Len()-1))
+	}
+	return
+}
+
+// Deletes the element of the slice at a given index.
+// The returned slice is a reference to the modified original array.
+//
+// The returned slice will be the same type as the given slice, but
+// stored in an interface{}.
+//
+// Uses reflection to perform this action on slices of any type.
+// Will panic if:
+//	- slice argument is not a slice type
+//	- idx is not in the range 0 .. len(slice)-1
+//
+// Equivalent to:
+//	slice = append(slice[:idx],slice[idx+1:]...)
+//
+// NOTE: Any other slices (including the argument slice)
+// referencing the original underlying array may have their contents
+// altered by this call. This behaviour is consistent with append().
+func Delete(slice interface{}, idx int) interface{} {
+	slice_val := check_delete(slice, idx)
+	begin := slice_val.Slice(0, idx)
+	end := slice_val.Slice(idx+1, slice_val.Len())
+	return reflect.AppendSlice(begin, end).Interface()
+}
+
+// Deletes the element of the slice at a given index.
+// The returned slice references a new array, the original
+// array and all referencing slices are un-altered.
+//
+// The returned slice will be the same type as the given slice, but
+// stored in an interface{}.
+//
+// Uses reflection to perform this action on slices of any type.
+// Will panic if:
+//	- slice argument is not a slice type
+//	- idx is not in the range 0 .. len(slice)-1
+// 
+// Equivalent to:
+//	slice = append(append(make([]T,0,len(slice)-1),slice[:idx]...),slice[idx+1]...)
+func DeleteCopy(slice interface{}, idx int) interface{} {
+	slice_val := check_delete(slice, idx)
+	begin := slice_val.Slice(0, idx)
+	end := slice_val.Slice(idx+1, slice_val.Len())
+	tmp := reflect.MakeSlice(slice_val.Type(), 0, slice_val.Len()-1)
+	return reflect.AppendSlice(reflect.AppendSlice(tmp, begin), end).Interface()
+}
---
+++ slice/container_test.go
@@ -0,0 +1,84 @@
+package slice
+
+import (
+	"reflect"
+	. "testing"
+)
+
+var insertTests = []struct {
+	Slice  interface{}
+	Index  int
+	Item   interface{}
+	Result interface{}
+}{
+	{[]int{}, 0, 1, []int{1}},
+	{[]int{}, 1, 1, nil},
+	{[]int{1, 2, 3}, 0, -99, []int{-99, 1, 2, 3}},
+	{[]int{1, 2, 3}, 1, 50, []int{1, 50, 2, 3}},
+	{[]int{1, 2, 3}, 3, 99, []int{1, 2, 3, 99}},
+	{[]int{1, 2, 3}, 2, "q", nil},
+	{5,2,1,nil},
+}
+
+type insertFunc func(interface{}, int, interface{}) interface{}
+
+func runInsertTest(t *T, slice interface{}, idx int, item interface{}, result interface{}, f insertFunc, name string) {
+	defer func() {
+		need_panic := result == nil
+		if need_panic {
+			if err := recover(); err == nil {
+				t.Error("\tDid not get expected panic")
+			} else {
+				t.Logf("\tGot expected panic: %v", err)
+			}
+		} else {
+			if err := recover(); err != nil {
+				t.Errorf("\tUnexpected panic: %v", err)
+			}
+		}
+	}()
+
+	t.Logf("Running test %s(%#v, %d, %#v) = %#v", name, slice, idx, item, result)
+	r := f(slice, idx, item)
+
+	if !reflect.DeepEqual(r, result) {
+		t.Errorf("\tUnexpected result: %v", r)
+	} else {
+		t.Logf("\tGot expected result: %v", r)
+	}
+}
+
+func TestInsert(t *T) {
+	for _, test := range insertTests {
+		runInsertTest(t, test.Slice, test.Index, test.Item, test.Result, Insert, "Insert")
+	}
+
+	for _, test := range insertTests {
+		runInsertTest(t, test.Slice, test.Index, test.Item, test.Result, InsertCopy, "InsertCopy")
+	}
+}
+
+func BenchmarkInsert(b *B) {
+	arr := []int{1, 2, 3, 4}
+
+	for i := 0; i < b.N; i++ {
+		_ = Insert(arr, 1, 2).([]int)
+	}
+}
+
+func BenchmarkInsertCopy(b *B) {
+	arr := []int{1, 2, 3, 4}
+
+	for i := 0; i < b.N; i++ {
+		_ = InsertCopy(arr, 1, 2).([]int)
+	}
+}
+
+func BenchmarkInsertBuiltin(b *B) {
+	arr := []int{1, 2, 3, 4}
+
+	for i := 0; i < b.N; i++ {
+		brr := append(arr[:2], arr[1:]...)
+		brr[1] = 2
+	}
+}
