b270301016642d7c7a0dc04859953b0290e221db
Carlos Castillo <cookieo9@gmail.com>
2012-10-20 20:00:59 -0700

    slice: added Reverse() function which reverses the order of items in slices

---
+++ slice/reverse.go
@@ -0,0 +1,18 @@
+package slice
+
+import (
+	"reflect"
+)
+
+// Reverse() reverses the items of the given slice (or array).
+// Will panic if not passed a slice or array.
+func Reverse(slice interface{}) {
+	v := reflect.ValueOf(slice)
+	l := v.Len()
+	for i := 0; i < l/2; i++ {
+		a, b := v.Index(i), v.Index(l-1-i)
+		t := a.Interface()
+		a.Set(b)
+		b.Set(reflect.ValueOf(t))
+	}
+}
---
+++ slice/reverse_test.go
@@ -0,0 +1,41 @@
+package slice
+
+import (
+	"reflect"
+	. "testing"
+)
+
+var tests = [][2][]int{
+	{{3}, {3}},
+	{{3, 1}, {1, 3}},
+	{{3, 1, 4}, {4, 1, 3}},
+	{{3, 1, 4, 1, 5, 9}, {9, 5, 1, 4, 1, 3}},
+	{{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}, {5, 3, 5, 6, 2, 9, 5, 1, 4, 1, 3}},
+}
+
+func TestReverse(t *T) {
+	for _, test := range tests {
+		input, expected := test[0], test[1]
+		output := make([]int, len(input))
+		copy(output, input)
+		Reverse(output)
+
+		t.Log("Reversing", input)
+		if !reflect.DeepEqual(output, expected) {
+			t.Error("Expected", expected, "got", output)
+		}
+	}
+}
+
+func BenchmarkReverse(b *B) {
+	a := make([]int, b.N)
+	Reverse(a)
+}
+
+func BenchmarkReverseRaw(b *B) {
+	a := make([]int, b.N)
+	for i := 0; i < len(a)/2; i++ {
+		i2 := len(a) - 1 - i
+		a[i], a[i2] = a[i2], a[i]
+	}
+}
