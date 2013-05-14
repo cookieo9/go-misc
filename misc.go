bc7bcba9f7bf5b917c17d8bd536ca8b34ce71e3b
Carlos Castillo <cookieo9@gmail.com>
2013-05-13 20:15:32 -0700

    Importing subrepos makes no sense.

--- misc.go
+++ misc.go
@@ -1,11 +1,2 @@
 // Some useful packages I have built for myself and others to look at and possibly use.
 package misc
-
-import (
-	_ "big128"
-	_ "dbg"
-	_ "flagutil"
-	_ "maps"
-	_ "pp"
-	_ "slice"
-)
