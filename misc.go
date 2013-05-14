e247794849a1c508dcd60cfeba20cbf5f17afe49
Carlos Castillo <cookieo9@gmail.com>
2013-05-13 20:13:17 -0700

    Made top level package automatically import subpackages.

--- misc.go
+++ misc.go
@@ -1,2 +1,11 @@
 // Some useful packages I have built for myself and others to look at and possibly use.
 package misc
+
+import (
+	_ "big128"
+	_ "dbg"
+	_ "flagutil"
+	_ "maps"
+	_ "pp"
+	_ "slice"
+)
