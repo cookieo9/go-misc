06be8d09eb83709b92cd95eedf651ca3c178d08d
Carlos Castillo <cookieo9@gmail.com>
2013-06-21 15:53:23 -0700

    cgo: added documentation

--- cgo/wrap.go
+++ cgo/wrap.go
@@ -1,3 +1,4 @@
+// Utility functions for CGO.
 package cgo
 
 import (
@@ -51,14 +52,39 @@ func make_slice(ptr *C.char, size int) (buf []byte) {
 	return
 }
 
+// Identical to cgo.WrapReadWriter, except providing
+// read-only services.
 func WrapReader(r io.Reader) (unsafe.Pointer, error) {
 	return wrapReadWriter(r)
 }
 
+// Identical to cgo.WrapReadWriter, except providing
+// write-only services.
 func WrapWriter(w io.Writer) (unsafe.Pointer, error) {
 	return wrapReadWriter(w)
 }
 
+// Wraps an io.ReadWriter into a libc *FILE. If the reader
+// supports the io.Seeker, or io.Closer interfaces, then
+// the extra functionality will be provided through the
+// generated *FILE pointer
+//
+// Even if the reader doesn't implement a Close()
+// method, you must still call fclose() on the FILE
+// pointer to clean up resources. If you don't want
+// the go version to be closed, you'll have to provide
+// a version of the object without a Close method, or
+// has one which does nothing.
+//
+// If you write to the *FILE, the write may be buffered
+// by libc, so remember to call fflush, or fclose,
+// to see changes in go.
+//
+// This function returns an unsafe.Pointer, because it must
+// be cast to a *C.FILE by the code using it. Returning a
+// *C.FILE in this package would require a user of this
+// package to convert it from a "*cgo.C.FILE" to a
+// "mypkg.C.FILE" with an unsafe.Pointer as a stepping stone.
 func WrapReadWriter(rw io.ReadWriter) (unsafe.Pointer, error) {
 	return wrapReadWriter(rw)
 }
