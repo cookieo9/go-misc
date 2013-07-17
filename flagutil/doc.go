85a71c8beafe9abced6aa27e73ff243c0da77461
Carlos Castillo <cookieo9@gmail.com>
2013-07-17 00:40:36 -0700

    flagutil: fixed package docs to match golint recommendations

--- flagutil/doc.go
+++ flagutil/doc.go
@@ -1,6 +1,2 @@
-// Extra flag types for the built-in flag library.
-//
-// flagutil provides new Functions for adding flags to the
-// default command line parser (flag.Parse()) as well as
-// to a custom *flag.FlagSet.
+// Package flagutil provides extra flag types for the built-in flag library.
 package flagutil
