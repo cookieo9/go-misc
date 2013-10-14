74d04f3c50f865bedc4bf2a5f65160b7682f03bf
Carlos Castillo <cookieo9@gmail.com>
2013-10-13 17:21:47 -0700

    go-misc: clean up golint's objection to top level package

--- misc.go
+++ misc.go
@@ -1,3 +1,3 @@
-// Some useful packages I have built for myself and others to look at
-// and possibly use.
+// Pacakge misc contains some useful packages I have
+// built for myself and others to look at and possibly use.
 package misc
